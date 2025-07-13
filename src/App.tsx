df678e9a30c4fb41d9a872a9b5a2e91a820735c4
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-07-13 13:35:49 -0700

    added Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Alert from "./components/Alert";
 
-import ListGroup from "./components/ListGroup";
 
 function App() {
+  
   return (
     <div>
-      <ListGroup/>
+     <Alert>
+      Hello World
+     </Alert>
     </div>
   )
 }
